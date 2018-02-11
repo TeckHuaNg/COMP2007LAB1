f431b218046c2ece725d74198699926de84d9553
TeckHuaNg <200348264@student.georgianc.on.ca>
2018-02-11 16:26:43 -0500

    initial commit

---
+++ Lab1/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Lab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = new List<Product>();
+            products.Add(new Product { Name = "Laptop" });
+            products.Add(new Product { Name = "Tablet" });
+            products.Add(new Product { Name = "Desktop" });
+
+            return View(products);
+        }
+
+        public ActionResult Details(string name)
+        {
+            ViewBag.Detail = "You selected product: " + name;
+
+            return View();
+        }
+
+
+    }
+}
